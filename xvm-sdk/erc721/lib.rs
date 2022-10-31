7f224fdcb8dd70ca8f288e7da442d817f29b8405
PierreOssun <pierre.giraud@mailfence.com>
2022-10-31 17:21:22 +0100

    added transfer_from

--- xvm-sdk/erc721/lib.rs
+++ xvm-sdk/erc721/lib.rs
@@ -8,13 +8,16 @@ pub use self::erc721::{
 use ink_lang as ink;
 
 /// EVM ID (from astar runtime)
-const EVM_ID: u8 = 0x1F;
+const EVM_ID: u8 = 0x0F;
 
 /// The EVM ERC721 delegation contract.
 #[ink::contract(env = xvm_sdk::XvmDefaultEnvironment)]
 mod erc721 {
-    const IS_APPROVED_FOR_ALL_SELECTOR: [u8; 4] = hex!["todo"];
+    const APPROVE_SELECTOR: [u8; 4] = hex!["095ea7b3"];
+    const TRANSFER_FROM_SELECTOR: [u8; 4] = hex!["095ea7b3"];
+    const MINT_SELECTOR: [u8; 4] = hex!["40c10f19"];
 
+    use alloc::vec::Vec;
     use ethabi::{ethereum_types::{H160, U256}, Token};
     use ink_prelude::vec::Vec;
     use hex_literal::hex;
@@ -25,7 +28,7 @@ mod erc721 {
     }
 
     impl Erc721 {
-        /// Create new ERC20 abstraction from given contract address.
+        /// Create new ERC721 abstraction from given contract address.
         #[ink(constructor)]
         pub fn new(evm_address: [u8; 20]) -> Self {
             Self {
@@ -33,23 +36,17 @@ mod erc721 {
             }
         }
 
-        // For now we can't handle methods returning complex values
-        // #[ink(message)]
-        // pub fn balance_of(&self, owner: [u8; 20]) -> u128 {
-        //     let encoded_input = Self::balance_of_encode(owner.into());
-        // }
-
-        // fn balance_of_encode(to: H160, value: U256) -> Vec<u8> {
-        //     let input = [
-        //         Token::FixedBytes(BALANCE_OF_SELECTOR.to_vec()),
-        //         Token::Address(to),
-        //     ];
-        //     ethabi::encode(&input)
-        // }
-
         #[ink(message)]
-        pub fn approve(to: [u8; 20], tokenId: U256) {
-
+        pub fn transfer_from(&mut self, from: [u8; 20], to: [u8; 20], token_id: U256) {
+            let encoded_input = Self::transfer_from_encode(from.into(), to.into(), token_id.into());
+            self.env()
+                .extension()
+                .xvm_call(
+                    super::EVM_ID,
+                    Vec::from(self.evm_address.as_ref()),
+                    encoded_input,
+                )
+                .is_ok()
         }
 
         #[ink(message)]
@@ -57,39 +54,15 @@ mod erc721 {
 
         }
 
-        #[ink(message)]
-        pub fn is_approved_for_all(owner: [u8; 20], operator: [u8; 20]) -> bool {
+        fn transfer_from_encode(from: H160, to: H160, token_id: U256) -> Vec<u8> {
+            let mut encoded = TRANSFER_FROM_SELECTOR.to_vec();
             let input = [
-                Token::FixedBytes(IS_APPROVED_FOR_ALL_SELECTOR.to_vec()),
-                Token::Address(owner),
-                Token::Address(operator),
+                Token::Address(from),
+                Token::Address(to),
+                Token::Uint(token_id),
             ];
-            let input = ethabi::encode(&input);
-
-            self.env()
-                .extension()
-                .xvm_call(
-                    super::EVM_ID,
-                    Vec::from(self.evm_address.as_ref()),
-                    input,
-                )
-                .is_ok()
-        }
-
-    }
-
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-
-        const SAMPLE_CONTRACT: [u8; 20] = hex!["a10d3DBe7c28F46A90476B841B8509978e00B995"];
-        const SAMPLE_TO: [u8; 20] = hex!["7C32982c3e7Fb8B2AF6ABD3323583C6A41f921C5"];
-        const SAMPLE_FROM: [u8; 20] = hex!["d806D071365C9Cc3EE19873212A3E0D553Ab97Da"];
-        const SAMPLE_VALUE: u128 = 12353241324u128;
-
-        #[test]
-        fn arguments_encoding() {
-            
+            encoded.extend(&ethabi::encode(&input));
+            encoded
         }
     }
 }
